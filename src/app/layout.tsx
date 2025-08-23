41ca634285eed89f0a5cc112be5105efac7fdfc2
Joel Byomuhangi <jbyomuhangi@gmail.com>
2025-08-23 15:53:38 +0800

    Added "nextjs-toploader" for route change indication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import clsx from "clsx";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 
 import theme from "@/theme";
 import "./globals.css";
@@ -30,6 +31,8 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <body className={clsx(geistSans.variable, geistMono.variable)}>
+        <NextTopLoader />
+
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </AppRouterCacheProvider>
