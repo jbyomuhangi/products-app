ef5b99aa2de247d319770adaae776123bd42838a
Joel Byomuhangi <jbyomuhangi@gmail.com>
2025-08-23 22:09:33 +0800

    Removed unused code

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { ProductApiData, ProductApiResponse } from "@/app/types/product";
-import { ProductQuery } from "@/app/types/query-engine/product";
 import { DataLoader } from "@/app/utils/dataLoader";
 import { ProductQueryEngine } from "@/app/utils/query-engine/products";
 import isInteger from "@/utils/validationUtils/isNumber";
@@ -60,39 +59,3 @@ export const GET = async (request: NextRequest) => {
     return NextResponse.json(responseData, { status: 500 });
   }
 };
-
-export async function POST(request: NextRequest) {
-  try {
-    let body: ProductQuery | null = null;
-
-    if (request.headers.get("content-length") === "0") {
-      body = null;
-    } else {
-      body = await request.json();
-    }
-
-    const { filter, sort, pagination } = body ?? {};
-
-    const products = DataLoader.getProducts();
-    const queryEngine = new ProductQueryEngine(products);
-    const result = await queryEngine.query({
-      filter,
-      sort,
-      pagination,
-    });
-
-    const data = {
-      results: result.data,
-      pagination: result.pagination,
-      total: result.total,
-    };
-
-    return NextResponse.json({ data });
-  } catch (error) {
-    console.error("Error processing products query:", error);
-    return NextResponse.json(
-      { error: "Failed to process products query" },
-      { status: 500 }
-    );
-  }
-}
---
+++ src/app/attributes/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AttributesPage = () => {
+  return <div>page</div>;
+};
+
+export default AttributesPage;
